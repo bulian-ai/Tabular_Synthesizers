064b7f27af09f0fbe9d27b6efd50e95aba65cf62
Rajneesh-Tiwari <rajneesh.vish1@gmail.com>
2022-01-08 09:30:09 -0800

    setup.py updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 with open('README.md', encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md', encoding='utf-8') as history_file:
-    history = history_file.read()
+# with open('HISTORY.md', encoding='utf-8') as history_file:
+#     history = history_file.read()
 
 install_requires = [
     'packaging>=20,<22',
