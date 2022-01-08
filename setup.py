70c90a88b3fe0df6ca6d41ad74e02957cf5042d6
Rajneesh-Tiwari <rajneesh.vish1@gmail.com>
2022-01-08 10:16:11 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -98,7 +98,6 @@ setup(
         'Development Status :: 1 - Pre-Alpha',
         'Intended Audience :: Developers/Data Scientists/Managers',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
