fbc4cf056f432026e244cda4f2bfb77fd1d313d9
Rajneesh-Tiwari <rajneesh.vish1@gmail.com>
2022-01-08 09:52:34 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ with open('README.md', encoding='utf-8') as readme_file:
 #     history = history_file.read()
 
 install_requires = [
+    'llvmlite==0.37.0',
     'packaging>=20,<22',
     "numpy>=1.18.0,<1.20.0;python_version<'3.7'",
     "numpy>=1.20.0,<2;python_version>='3.7'",
