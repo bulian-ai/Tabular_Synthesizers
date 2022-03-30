1e8581098b557087217e268b139f73e38df938bd
Rajneesh-Tiwari <rajneesh.vish1@gmail.com>
2022-03-30 20:08:00 +0530

    docs

--- bulian/synthesizers/__init__.py
+++ bulian/synthesizers/__init__.py
@@ -1,7 +1,2 @@
 from .twin_synthesizer import TwinSynthesizer
 from .private_twin_synthesizer import PrivateTwinSynthesizer
-
-__all__ = [
-    'twin_synthesizer',
-    'private_twin_synthesizer',
-]
