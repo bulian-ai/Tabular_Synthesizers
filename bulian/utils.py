c57fbf7f4fcd166a6c91c4c11436640003435066
Rajneesh Tiwari <rajneesh.vish1@gmail.com>
2022-05-12 12:15:18 +0530

    utils func

--- bulian/utils.py
+++ bulian/utils.py
@@ -96,6 +96,14 @@ class MetricLogger:
             loss_str.append(f"{name}: {str(meter)}")
         return self.delimiter.join(loss_str)
 
+    def global_avg(self):
+        loss_str = []
+        for name, meter in self.meters.items():
+            loss_str.append(
+                "{}: {:.4f}".format(name, meter.global_avg)
+            )
+        return self.delimiter.join(loss_str) 
+        
     def synchronize_between_processes(self):
         for meter in self.meters.values():
             meter.synchronize_between_processes()
