9473ca8ba9f1163c50f016efd35c9bbe9f719175
Rajneesh-Tiwari <rajneesh.vish1@gmail.com>
2022-07-18 18:15:33 +0530

    setup.py

--- bulian/Tabular/synthesizers/__init__.py
+++ bulian/Tabular/synthesizers/__init__.py
@@ -1,3 +1,7 @@
 from .twin_synthesizer import TwinSynthesizer
 from .private_twin_synthesizer import PrivateTwinSynthesizer
 from .copula_synth import GaussianCopula
+
+__all__ = (
+    'TwinSynthesizer','PrivateTwinSynthesizer','GaussianCopula'
+)
